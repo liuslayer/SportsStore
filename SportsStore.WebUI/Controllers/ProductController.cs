630b7074de48bcdffd833c5980f128b4ba4b01d0
liuslayer <359296156@qq.com>
2017-09-11 17:36:24 +0800

    Initial commit

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize = 4;
+        //
+        // GET: /Product/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            return View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
+        }
+	}
+}
