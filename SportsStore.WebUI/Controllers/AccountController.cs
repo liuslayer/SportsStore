4ba7f02799b4237332436e035f3cab6519f68403
liuslayer <359296156@qq.com>
2017-09-21 17:59:51 +0800

    安全性验证

---
+++ SportsStore.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IAuthProvider authProvider;
+ 
+        public AccountController(IAuthProvider auth)
+        {
+            this.authProvider = auth;
+        }
+
+        //
+        // GET: /Account/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (authProvider.Authenticate(model.UserName, model.Password))
+                {
+                    return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "用户名或密码错误");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+	}
+}
